b4f1a6528268fa78b880aab2fd52cab29877ea96
nico <nicolo@eebria.com>
2018-11-22 10:31:15 +0000

    Version bump, fix description

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='wargery',
-    version='1.0',
-    description='Create a sensibly named war artifact for a grails project',
+    version='1.1',
+    description='Create a sensibly named war artifact from a Grails project',
     author='Nicolo Maioli',
     author_email='nicolomaioli@gmail.com',
     packages=find_packages(),
