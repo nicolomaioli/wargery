5697c85c78a6bfa3e8de260ac7c5f9a9d1a5ff23
Nicolo Maioli <nicolo@eebria.com>
2019-02-08 14:29:25 +0000

    Fix versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wargery',
-    version='1.2',
+    version='1.2.0',
     description='Create a sensibly named war artifact from a Grails project',
     author='Nicolo Maioli',
     author_email='nicolomaioli@gmail.com',
